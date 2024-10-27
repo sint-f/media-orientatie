656ac35d68de65baa7e6d6ac6426f61c8f6f5e3b
Fmt2022 <f.puts@sintlucasedu.nl>
2024-10-27 15:11:27 +0100

    change img alt

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,7 @@
 <main class="bg-green rounded-b-lg drop-shadow-lg sticky">
     <div class="flex justify-center">
         <input class="my-5 mx-1 w-96 h-10 rounded-full" type="text">
-        <img src="../../public/minimal-48-512%201.png" class="my-6 mr-1 ml-[-2.5rem] h-8">
-        <img src="../../public/230458-200%201.png" class="my-6 mx-1 pr-[-1rem] h-8">
+        <img src="public/minimal-48-512%201.png" alt="search" class="my-6 mr-1 ml-[-2.5rem] h-8">
+        <img src="public/230458-200%201.png" alt="settings" class="my-6 mx-1 pr-[-1rem] h-8">
     </div>
 </main>
